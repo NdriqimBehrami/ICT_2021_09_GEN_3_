6579a0f62f36b7719f7de9ffab045de0dc936bae
Ndriqim <ndriqimib@gmail.com>
2021-10-03 23:03:40 +0200

    Numrat me rend katrore

---
+++ src/ict/kosovo/growth_/basic/ora_5/NumratMeRendKatror.java
@@ -0,0 +1,13 @@
+package ict.kosovo.growth_.basic.ora_5;
+
+public class NumratMeRendKatror {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Numri : " + i);
+            System.out.println("Katrori i numrit " + i + " eshte:" + i * i);
+            System.out.println("-------------------------");
+        }
+
+    }
+}
