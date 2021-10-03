e0ad080944ed476f64e70296e21ce2073e54a502
Ndriqim <ndriqimib@gmail.com>
2021-10-03 23:12:53 +0200

    Tabela e shumzimit

---
+++ src/ict/kosovo/growth_/basic/ora_5/TabelaEShumzimit.java
@@ -0,0 +1,16 @@
+package ict.kosovo.growth_.basic.ora_5;
+
+import java.util.Scanner;
+
+public class TabelaEShumzimit {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Shtypni nje numer per te gjeneruar tabelen e shumzimit nga 1 deri ne 10");
+        int numri = reader.nextInt();
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%d * %d = %d \n", numri, i, numri * i);
+        }
+
+    }
+}
