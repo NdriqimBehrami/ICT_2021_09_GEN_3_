c3c41f93fecdd1d7c9413998a4423da22dfc29ae
Ndriqim <ndriqimib@gmail.com>
2021-10-04 22:55:32 +0200

    top 10 friends with array

---
+++ src/ict/kosovo/growth_/basic/ora_6/DetyraMe10TopFriends.java
@@ -0,0 +1,17 @@
+package ict.kosovo.growth_.basic.ora_6;
+
+import java.util.Scanner;
+
+public class DetyraMe10TopFriends {
+    public static void main(String[] args) {
+      String [] friends = {"Jon", "Nik", "Klaus","Elijah","Noah", "Elena","Jooy","Kai", "Tyrion"};
+      for (int i = 3; i< friends.length;i++) {
+          System.out.println(friends[i]);
+      }
+
+
+
+    }
+
+
+}
