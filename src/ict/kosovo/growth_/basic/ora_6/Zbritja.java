351ad998d3ab8c941283fd8c3ed78db34d52b8dc
Ndriqim <ndriqimib@gmail.com>
2021-10-06 13:45:35 +0200

    Metoda zbritja

--- src/ict/kosovo/growth_/basic/ora_6/Zbritja.java
+++ src/ict/kosovo/growth_/basic/ora_6/Zbritja.java
@@ -1,14 +1,13 @@
 package ict.kosovo.growth_.basic.ora_6;
 
 public class Zbritja {
+
     public static void main(String[] args) {
-        double x = 10;
-        double y = 20;
-        System.out.println(zbritja(x,y));
+        zbritja(11, 8);
 
     }
 
-    private static double zbritja(double x, double y) {
-        return x < y ? x : y;
+    public static void zbritja(double a, double b) {
+        System.out.println(a - b);
     }
 }
