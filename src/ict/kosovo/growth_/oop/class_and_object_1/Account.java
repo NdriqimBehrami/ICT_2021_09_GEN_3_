354e8e3004c4eecd95f943a8bb744030425948a4
Ndriqim <ndriqimib@gmail.com>
2021-10-08 00:52:34 +0200

    Account test

---
+++ src/ict/kosovo/growth_/oop/class_and_object_1/Account.java
@@ -0,0 +1,15 @@
+package ict.kosovo.growth_.oop.class_and_object_1;
+
+public class Account {
+    private String name;
+
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/ict/kosovo/growth_/oop/class_and_object_1/AccountTest.java
@@ -0,0 +1,13 @@
+package ict.kosovo.growth_.oop.class_and_object_1;
+
+public class AccountTest {
+    public static void main(String[] args) {
+
+        Account ndriqimisAccount = new Account();
+
+        ndriqimisAccount.setName("Ndriqim Behrami");
+
+        System.out.println(ndriqimisAccount.getName());
+
+    }
+}
