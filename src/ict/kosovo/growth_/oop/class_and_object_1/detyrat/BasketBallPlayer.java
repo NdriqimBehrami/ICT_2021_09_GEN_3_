9447cc100c4fc3839fc892a6e3e98caa48115b2d
Ndriqim <ndriqimib@gmail.com>
2021-10-08 15:45:27 +0200

    Detyra e dyte klasa BasketBallPlayer

---
+++ src/ict/kosovo/growth_/oop/class_and_object_1/detyrat/BasketBallPlayer.java
@@ -0,0 +1,98 @@
+package ict.kosovo.growth_.oop.class_and_object_1.detyrat;
+
+public class BasketBallPlayer {
+    private String emri;
+    private String mbiemri;
+    private int numri;
+    private String ekipiLuajtes;
+    private double gjatsia;
+    private double preciziteti;
+    private double kercimiLart;
+    private double shpejtsia;
+    private String pozita;
+    private int shitja;
+
+    public BasketBallPlayer(){
+        //vlerat
+    }
+
+    public String getEmri() {
+        return emri;
+    }
+
+    public void setEmri(String emri) {
+        this.emri = emri;
+    }
+
+    public String getMbiemri() {
+        return mbiemri;
+    }
+
+    public void setMbiemri(String mbiemri) {
+        this.mbiemri = mbiemri;
+    }
+
+    public int getNumri() {
+        return numri;
+    }
+
+    public void setNumri(int numri) {
+        this.numri = numri;
+    }
+
+    public String getEkipiLuajtes() {
+        return ekipiLuajtes;
+    }
+
+    public void setEkipiLuajtes(String ekipiLuajtes) {
+        this.ekipiLuajtes = ekipiLuajtes;
+    }
+
+    public double getGjatsia() {
+        return gjatsia;
+    }
+
+    public void setGjatsia(double gjatsia) {
+        this.gjatsia = gjatsia;
+    }
+
+    public double getPreciziteti() {
+        return preciziteti;
+    }
+
+    public void setPreciziteti(double preciziteti) {
+        this.preciziteti = preciziteti;
+    }
+
+    public double getKercimiLart() {
+        return kercimiLart;
+    }
+
+    public void setKercimiLart(double kercimiLart) {
+        this.kercimiLart = kercimiLart;
+    }
+
+    public double getShpejtsia() {
+        return shpejtsia;
+    }
+
+    public void setShpejtsia(double shpejtsia) {
+        this.shpejtsia = shpejtsia;
+    }
+
+    public String getPozita() {
+        return pozita;
+    }
+
+    public void setPozita(String pozita) {
+        this.pozita = pozita;
+    }
+
+    public int getShitja() {
+        return shitja;
+    }
+
+    public void setShitja(int shitja) {
+        this.shitja = shitja;
+    }
+}
---
+++ src/ict/kosovo/growth_/oop/class_and_object_1/detyrat/BasketBallPlayerTest.java
@@ -0,0 +1,22 @@
+package ict.kosovo.growth_.oop.class_and_object_1.detyrat;
+
+public class BasketBallPlayerTest {
+    public static void main(String[] args) {
+        BasketBallPlayer newplayer = new BasketBallPlayer();
+        newplayer.setEmri("Lebron");
+        newplayer.setMbiemri("James");
+        newplayer.setNumri(23);
+        newplayer.setEkipiLuajtes("Lakers");
+        newplayer.setGjatsia(2.07);
+        newplayer.setPreciziteti(9.9);
+        newplayer.setKercimiLart(3.2);
+        newplayer.setShpejtsia(8.2);
+        newplayer.setPozita("Sulm");
+        newplayer.setShitja(50);
+        System.out.printf("Lojtari %s %s me numrin %d ne ekipin %s %n me gjatsi %.2f dhe precizitet %.2f me kercim lart %.2f %n"  +
+                "me shpejtsi per meter %.2f ne poziten ku luan %s qe ne shitje doli me cmimin ne miliomn %d %n",newplayer.getEmri(),newplayer.getMbiemri()
+        ,newplayer.getNumri(),newplayer.getEkipiLuajtes(),newplayer.getGjatsia(),newplayer.getPreciziteti(),newplayer.getKercimiLart(),newplayer.getShpejtsia()
+        ,newplayer.getPozita(),newplayer.getShitja());
+    }
+
+}
