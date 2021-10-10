59db946ad72fe1e83a6fc2a22477b5e453e4e788
Ndriqim <ndriqimib@gmail.com>
2021-10-10 22:34:13 +0200

    Detyra me pixels

---
+++ src/ict/kosovo/growth_/oop/ushtrime3/Pixel.java
@@ -0,0 +1,71 @@
+package ict.kosovo.growth_.oop.ushtrime3;
+
+public class Pixel {
+    private double x;
+    private double y;
+    private double lattiude;
+    private double longitude;
+    private double zoom;
+    private DistanceUnit distance;
+    private String ngjyra;
+
+    public Pixel (double x, double y,String ngjyra){
+        this.x = x;
+        this.y = y;
+        this.ngjyra=ngjyra;
+        this.distance = DistanceUnit.KM;
+    }
+
+    public int getX() {
+        return (int) x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return (int) y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public DistanceUnit getDistanca() {
+        return distance;
+    }
+
+    public void setDistanca(DistanceUnit distanca) {
+        this.distance = distance;
+    }
+
+    public String getNgjyra() {
+        return ngjyra;
+    }
+
+    public void setNgjyra(String ngjyra) {
+        this.ngjyra = ngjyra;
+    }
+
+    public double distantTo(Pixel vetura2){
+        double distance = Math.toRadians(y - x);
+        return distance;
+
+    }
+
+    public DistanceUnit getDistance() {
+        return distance;
+    }
+
+
+    public double getgpsCoordinates() {
+        double koordinata =  Math.cos(Math.toRadians(x))
+                * Math.cos(Math.toRadians(y))
+                * Math.cos(Math.toRadians((y) - (x)))
+                + Math.sin(Math.toRadians(x))
+                * Math.sin(Math.toRadians(y));
+        return koordinata;
+    }
+}
+
---
+++ src/ict/kosovo/growth_/oop/ushtrime3/PixelTest.java
@@ -0,0 +1,15 @@
+package ict.kosovo.growth_.oop.ushtrime3;
+
+public class PixelTest {
+    public static void main(String[] args) {
+        Pixel vetura1 = new Pixel(41.40338, 2.17403,"kuqe");
+        Pixel vetura2 = new Pixel(6.13303, 36.55510,"gjelbert");
+
+        System.out.printf("Distance eshte: %.3f %s", vetura1.distantTo(vetura2), vetura1.getDistance());
+        System.out.println();
+        System.out.println("Koordinatat per veteuren e pare jane: " +vetura1.getgpsCoordinates());
+        System.out.printf("Koordinatat e dy veturave jane: %.2f --- %.2f %n",vetura1.getgpsCoordinates(),vetura2.getgpsCoordinates());
+        System.out.println("Distanca ndermjet vetures se pare me ngjyre te " + vetura1.getNgjyra()+" dhe vetures se dyte me ngjyre te "+ vetura2.getNgjyra()+ " eshte: "+vetura1.distantTo(vetura2));
+        System.out.println("ngjyra eshte "+ vetura1.getNgjyra());
+    }
+}
